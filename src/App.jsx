b24c382f0fd1975a5032fdfedc922421e321860b
hasanalit <tuxtayevhasan72@gmail.com>
2022-07-28 10:06:21 +0500

    initial-commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Button} from 'react-bootstrap'
+
+function App() {
+  return (
+    <>
+    <Button variant='primary'>Submit</Button>
+    <div className='d-flex ms-3'>
+      <p>text</p>
+      <p className='ms-3'>text</p>
+    </div>
+    </>
+  );
+}
+
+export default App;
